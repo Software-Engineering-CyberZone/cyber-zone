1abe563c99ec9dfbc240f157c6cd2824be8f64d9
Віталій <vityadomanov@gmail.com>
2026-03-18 02:18:12 +0200

    2 new feature test

---
+++ CyberZone.Application/Interfaces/IClubService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberZone.Application.Interfaces
+{
+    internal class IClubService
+    {
+    }
+}
